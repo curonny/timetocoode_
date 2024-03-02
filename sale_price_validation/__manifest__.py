e0f7bd25f26cb845f78a5339956121adfa58798f
Ronny Montano <rmontano1992@gmail.com>
2024-03-02 17:24:03 -0600

    Add description

--- sale_price_validation/__manifest__.py
+++ sale_price_validation/__manifest__.py
@@ -5,8 +5,9 @@
     "author": "Ronny Montano <<rmontano1992@gmail.com>>",
     "category": "Sale",
     "version": "17.0.0.1.0",
+    "license": "OPL-1",
+    "price": 8,
+    "currency": "EUR",
     "depends": ["sale", "sale_management"],
-    "data": [
-    ],
 
 }
--- sale_price_validation/models/sale_order_line.py
+++ sale_price_validation/models/sale_order_line.py
@@ -7,10 +7,9 @@ class SaleOrderLine(models.Model):
 
     @api.constrains('price_unit')
     def _constrains_price_unit(self):
-        # Use for (If not use for, the pattern singlenton should be launched)
+        # Use for (If not use for, the pattern singleton should be launched)
         # The price unit should greater than standard price + 15%
 
-        #READY
         for line in self:
             print(line.product_id.standard_price * 1.15)
             if line.price_unit < (line.product_id.standard_price * 1.15):
