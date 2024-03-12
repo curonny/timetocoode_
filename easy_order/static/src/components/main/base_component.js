aab5331ac155411de424cd06736b42863915b69f
Ronny Montano <rmontano1992@gmail.com>
2024-03-11 23:18:22 -0600

    Implement cart shopping

--- easy_order/static/src/components/main/base_component.js
+++ easy_order/static/src/components/main/base_component.js
@@ -12,6 +12,7 @@ import { useService } from "@web/core/utils/hooks";
 //UseState is a hook, defined to add reactivity to components
 import { Component, useSubEnv, useState,useRef,onWillStart } from "@odoo/owl";
 import { EasyOrderProducts } from "../products/products_component";
+import { EasyOrderProductsShoppingCart } from "../car/car_component";
 
 // Easy Order will be a actions client to create purchase order and sale order more easy and quick
 // just like point of sale
@@ -25,11 +26,13 @@ export class EasyOrder extends Component {
         ...this.env.config,
       },
     });
-    const notificationService = useService("notification");
+    this.notificationService = useService("notification");
 
-    notificationService.add("Hello, welcome to order easy creator");
+    this.notificationService.add("Hello, welcome to order easy creator");
     this.state = useState({
+        amount_total:0,
         products:[],
+        shoppingCart:[],
     })
     this.productModel = 'product.template'
     this.orm = useService("orm");
@@ -40,17 +43,40 @@ export class EasyOrder extends Component {
 
   }
     async getAllProducts(){
-        this.state.products = await this.orm.searchRead(this.productModel, [["detailed_type", "!=", "service"]], ["name", "list_price","standard_price"])
+        this.state.products = await this.orm.searchRead(this.productModel, [["detailed_type", "!=", "service"]], ["name", "list_price","standard_price","image_128"])
     }
     async searchProducts(){
         const textProduct = this.searchInput.el.value;
         this.state.products = await this.orm.searchRead(this.productModel, [["name", "ilike", textProduct]], ["name", "list_price","standard_price"])
     }
+
+    addToCard(productId){
+
+        const productIndex = this.state.products.findIndex(product => product.id === productId);
+        const productInCartIndex = this.state.shoppingCart.findIndex(product => product.id === productId);
+
+        if(productInCartIndex === -1){
+            this.state.shoppingCart.push({
+            "id":productId,
+            "name":this.state.products[productIndex].name,
+            "list_price":this.state.products[productIndex].list_price,
+            "standard_price":this.state.products[productIndex].standard_price,
+            "quantity":1,
+            "price_subtotal":this.state.products[productIndex].list_price*1
+            })
+        }
+        else{
+            this.state.shoppingCart[productInCartIndex].quantity += 1;
+            this.state.shoppingCart[productInCartIndex].price_subtotal = this.state.shoppingCart[productInCartIndex].list_price * this.state.shoppingCart[productInCartIndex].quantity;
+        }
+        this.state.amount_total = this.state.shoppingCart.reduce((total, product) => total + (product.price_subtotal), 0);
+        this.notificationService.add("Product "+ this.state.products[productIndex].name + " added to card");
+    }
 }
 
 // In this case, we are assigning a xml template to js component
 EasyOrder.template = "owl.EasyOrder";
-EasyOrder.components = {EasyOrderProducts};
+EasyOrder.components = {EasyOrderProducts,EasyOrderProductsShoppingCart};
 
 // With registry, and actions, yo can add ir.actions.client
 
--- easy_order/static/src/components/products/products_component.js
+++ easy_order/static/src/components/products/products_component.js
@@ -12,22 +12,21 @@ import { useService } from "@web/core/utils/hooks";
 //UseState is a hook, defined to add reactivity to components
 import { Component, useSubEnv, useState } from "@odoo/owl";
 
-// Easy Order will be a actions client to create purchase order and sale order more easy and quick
-// just like point of sale
-
-//EasyOrderProducts is the component name, when export is used the component is published to all modules that required it
 export class EasyOrderProducts extends Component {
-//In setup, load config..
-// will start creating a user welcome notification
-  setup() {
-    useSubEnv({
-      config: {
-        ...getDefaultConfig(),
-        ...this.env.config,
-      },
-    });
+static props = {
+    product:{
+        type: Object,
+        shape: {
+            id:Number,name:String,list_price:Number,standard_price:Number
+        },
+    },
+    addToCard: Function,
 
-  }
+};
+
+addToCard(){
+    this.props.addToCard(this.props.product.id);
+}
 
 }
 
